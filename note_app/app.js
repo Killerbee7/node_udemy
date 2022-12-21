04b0d4c3bd6a3431c89df394aa179ff516bd9a55
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-12-22 00:57:30 +0200

    validator and chalk added

--- note_app/app.js
+++ note_app/app.js
@@ -1,1 +1,15 @@
-console.log("Node")
+
+
+//Validator 
+
+const validator = require('validator')
+
+console.log(validator.isURL('https://google.com'));
+
+//Chalk 
+
+var chalk = require("chalk")
+
+const success = chalk.green('success');
+
+console.log(success);
