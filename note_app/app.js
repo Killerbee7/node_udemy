19532f607b07f951472cf631738f60554ab2dfb1
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-12-22 02:00:09 +0200

    yargs updates

--- note_app/app.js
+++ note_app/app.js
@@ -1,15 +1,70 @@
+//FS
 
+// const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', "Hello, i am learning node")
 
 //Validator 
 
 const validator = require('validator')
 
-console.log(validator.isURL('https://google.com'));
+// console.log(validator.isURL('https://google.com'));
 
 //Chalk 
 
-var chalk = require("chalk")
+const chalk = require('chalk')
+const yargs = require('yargs');
+const { describe } = require('yargs');
+// const success = chalk.green('success');
 
-const success = chalk.green('success');
+// console.log(success);
 
-console.log(success);
+// const command = process.argv[2]
+
+// if(command === 'add'){
+//     console.log(chalk.green('adding note....'))
+// }
+// else if(command === 'remove'){
+//     console.log(chalk.red('removing note.....'));
+// }
+
+// console.log(process.argv);
+
+
+//create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add new note',
+    handler: function(){
+        console.log('Adding new note.....')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function(){
+        console.log('Listing the notes...');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note...');
+    }
+})
+// create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log("Removing a note...")
+    }
+
+})
+
+console.log(yargs.argv);
